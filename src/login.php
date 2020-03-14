0a7eb397bef7a001c8efb3a28dafe6f9fc846e35
Dmytro <dlunhu@gmail.com>
2020-03-14 13:34:20 -0400

    Form array

--- src/login.php
+++ src/login.php
@@ -14,73 +14,74 @@ session_start();
     <div class="container">
         <!-- Content container here -->
         <!-- CSS jumbotron -->
-        <div class="jumbotron">
-            <h1 class="display-4">Hello, world!</h1>
-            <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention
-                to featured content or information.</p>
-            <hr class="my-4">
-            <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
-            <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
-            <!-- CSS FORM -->
-            <form>
-                <div class="form-group row">
-                    <label for="inputEmail3" class="col-sm-2 col-form-label">Email</label>
-                    <div class="col-sm-10">
-                        <input type="email" class="form-control" id="inputEmail3" placeholder="Email">
-                    </div>
+    </div>
+    <div class="jumbotron">
+        <h1 class="display-4">Hello, world!</h1>
+        <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention
+            to featured content or information.</p>
+        <hr class="my-4">
+        <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+        <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+        <!-- CSS FORM -->
+        <form>
+            <div class="form-group row">
+                <label for="inputEmail3" class="col-sm-2 col-form-label">Email</label>
+                <div class="col-sm-10">
+                    <input type="email" class="form-control" id="inputEmail3" placeholder="Email">
                 </div>
-                <div class="form-group row">
-                    <label for="inputPassword3" class="col-sm-2 col-form-label">Password</label>
-                    <div class="col-sm-10">
-                        <input type="password" class="form-control" id="inputPassword3" placeholder="Password">
-                    </div>
+            </div>
+            <div class="form-group row">
+                <label for="inputPassword3" class="col-sm-2 col-form-label">Password</label>
+                <div class="col-sm-10">
+                    <input type="password" class="form-control" id="inputPassword3" placeholder="Password">
                 </div>
-                <fieldset class="form-group">
-                    <div class="row">
-                        <legend class="col-form-label col-sm-2 pt-0">Radios</legend>
-                        <div class="col-sm-10">
-                            <div class="form-check">
-                                <input class="form-check-input" type="radio" name="gridRadios" id="gridRadios1"
-                                    value="option1" checked>
-                                <label class="form-check-label" for="gridRadios1">
-                                    First radio
-                                </label>
-                            </div>
-                            <div class="form-check">
-                                <input class="form-check-input" type="radio" name="gridRadios" id="gridRadios2"
-                                    value="option2">
-                                <label class="form-check-label" for="gridRadios2">
-                                    Second radio
-                                </label>
-                            </div>
-                            <div class="form-check disabled">
-                                <input class="form-check-input" type="radio" name="gridRadios" id="gridRadios3"
-                                    value="option3" disabled>
-                                <label class="form-check-label" for="gridRadios3">
-                                    Third disabled radio
-                                </label>
-                            </div>
-                        </div>
-                    </div>
-                </fieldset>
-                <div class="form-group row">
-                    <div class="col-sm-2">Checkbox</div>
+            </div>
+            <fieldset class="form-group">
+                <div class="row">
+                    <legend class="col-form-label col-sm-2 pt-0">Radios</legend>
                     <div class="col-sm-10">
                         <div class="form-check">
-                            <input class="form-check-input" type="checkbox" id="gridCheck1">
-                            <label class="form-check-label" for="gridCheck1">
-                                Example checkbox
+                            <input class="form-check-input" type="radio" name="gridRadios" id="gridRadios1"
+                                value="option1" checked>
+                            <label class="form-check-label" for="gridRadios1">
+                                First radio
+                            </label>
+                        </div>
+                        <div class="form-check">
+                            <input class="form-check-input" type="radio" name="gridRadios" id="gridRadios2"
+                                value="option2">
+                            <label class="form-check-label" for="gridRadios2">
+                                Second radio
+                            </label>
+                        </div>
+                        <div class="form-check disabled">
+                            <input class="form-check-input" type="radio" name="gridRadios" id="gridRadios3"
+                                value="option3" disabled>
+                            <label class="form-check-label" for="gridRadios3">
+                                Third disabled radio
                             </label>
                         </div>
                     </div>
                 </div>
-                <div class="form-group row">
-                    <div class="col-sm-10">
-                        <button type="submit" class="btn btn-primary">Sign in</button>
+            </fieldset>
+            <div class="form-group row">
+                <div class="col-sm-2">Checkbox</div>
+                <div class="col-sm-10">
+                    <div class="form-check">
+                        <input class="form-check-input" type="checkbox" id="gridCheck1">
+                        <label class="form-check-label" for="gridCheck1">
+                            Example checkbox
+                        </label>
                     </div>
                 </div>
-            </form>
-        </div>
+            </div>
+            <div class="form-group row">
+                <div class="col-sm-10">
+                    <button type="submit" class="btn btn-primary">Sign in</button>
+                </div>
+            </div>
+        </form>
+    </div>
     </div>
 
 
--- src/programmation-web-3.sql
+++ src/programmation-web-3.sql
@@ -1,30 +1,30 @@
--- SET SQL_MODE
--- ="NO_AUTO_VALUE_ON_ZERO";
+SET SQL_MODE
+="NO_AUTO_VALUE_ON_ZERO";
 
 
--- CREATE DATABASE `programmation-web-3` DEFAULT CHARACTER
--- SET latin1
--- COLLATE latin1_swedish_ci;
--- USE `programmation-web-3`;
+CREATE DATABASE `programmation-web-3` DEFAULT CHARACTER
+SET latin1
+COLLATE latin1_swedish_ci;
+USE `programmation-web-3`;
 
 
--- DROP TABLE IF EXISTS tp_user;
+DROP TABLE IF EXISTS tp_user;
 
--- CREATE TABLE tp_user
--- (
---     id INT(6)
---     UNSIGNED AUTO_INCREMENT PRIMARY KEY,
---     firstName VARCHAR
---     (30) NOT NULL,
---     lastName VARCHAR
---     (30) NOT NULL,
---     email VARCHAR
---     (50),
---     userName VARCHAR
---     (30) NOT NULL,
---     userPassword VARCHAR
---     (250) NOT NULL,
---     creationDate TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
---     modificationDate TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON
---     UPDATE CURRENT_TIMESTAMP
---     );
+CREATE TABLE tp_user
+(
+    id INT(6)
+    UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    firstName VARCHAR
+    (30) NOT NULL,
+    lastName VARCHAR
+    (30) NOT NULL,
+    email VARCHAR
+    (50),
+    userName VARCHAR
+    (30) NOT NULL,
+    userPassword VARCHAR
+    (250) NOT NULL,
+    creationDate TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    modificationDate TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON
+    UPDATE CURRENT_TIMESTAMP
+    );
