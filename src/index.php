32a88d691ef7f7174fc3095d377ab7403185cf43
Dmytro <dlunhu@gmail.com>
2020-03-15 15:48:51 -0400

    table

--- src/index.php
+++ src/index.php
@@ -21,38 +21,38 @@
                     <th scope="col">Courriel</th>
                     <th scope="col">Date de création</th>
                     <th scope="col">Date de modification</th>
-                    <th scope="col">$icone</th>
+                    <th scope="col"></th>
                 </tr>
             </thead>
             <tbody>
                 <tr>
                     <td scope="row">AAAA</td>
                     <td>BBBB</td>
-                    <td>Otto</td>
                     <td>@Courriel</td>
                     <td>date création</td>
                     <td>date de modification</td>
+                    <td>$icone</td>
                 </tr>
                 <tr>
                     <td scope="row">CCCC</td>
                     <td>DDDD</td>
-                    <td>Otto</td>
                     <td>@Courriel</td>
                     <td>date création</td>
                     <td>date de modification</td>
+                    <td>$icone</td>
                 </tr>
                 <tr>
                     <td scope="row">EEEE</td>
                     <td>FFFF</td>
-                    <td>Otto</td>
                     <td>@Courriel</td>
                     <td>date création</td>
                     <td>date de modification</td>
+                    <td>$icone</td>
                 </tr>
             </tbody>
         </table>
     </div>
-    <p>on est dans index.php </p>
+    <p>Page INDEX </p>
     <p> <a href='login.php'> Aller LOGIN.php </a> </p>
     <p> <a href='form.php'> Aller FORM.php </a> </p>
 </body>
